607cd0daa8276fae40c446ef66e86d3592ddaaa7
Xianggong <55724908+XiangGong233@users.noreply.github.com>
2021-08-16 21:28:07 -0400

    啊

--- project/portfolio.js
+++ project/portfolio.js
@@ -1,54 +1,66 @@
 
 console.log('javascript start')
 
+
+
+var projectinput = document.getElementById('projectinput')
+
+projectinput.onclick = function projectinputmove() {
+	console.log('projectinput moved to close');
+
+	if (projectinput.length < 1) {
+		projectinput.classList.add('movedclose');
+	} else {
+		projectinput.classList.remove('movedclose')
+	}
+
+}
+
 // ------------------------
 // https://codepen.io/Ruworks/pen/jPKZXR
 // https://codepen.io/gt-26/pen/Vbawmx
 // ---------------------
 
+// const dark = document.getElementById('dark');
+// const light = document.getElementById('light');
+// const main = document.getElementById('main');
+// const text = document.getElementById('text');
+// const pdtitle = document.getElementById('pdtitle1')
+// const topbox = document.getElementById('top')
+// const footer = document.getElementById('footer')
 
-const dark = document.getElementById('dark');
-const light = document.getElementById('light');
-const main = document.getElementById('main');
-const text = document.getElementById('text');
-const pdtitle = document.getElementById('pdtitle1')
-const topbox = document.getElementById('top')
-const footer = document.getElementById('footer')
+// dark.onclick = function themeDark() {
+// 	console.log('main');
+//     dark.classList.add('hidden');
+// 	light.classList.remove('hidden');
+// 	main.classList.add('darkcolor');
+// 	header002.classList.add('darkcolor');
 
-
-
-dark.onclick = function themeDark() {
-	console.log('main');
-    dark.classList.add('hidden');
-	light.classList.remove('hidden');
-	main.classList.add('darkcolor');
-	header002.classList.add('darkcolor');
-
-	text.classList.add('lightcolor');
-	pdtitle.classList.add('lightcolor');
-	topbox.classList.add('darkcolor');
-	footer.classList.add('darkcolorfooter');
+// 	text.classList.add('lightcolor');
+// 	pdtitle.classList.add('lightcolor');
+// 	topbox.classList.add('darkcolor');
+// 	footer.classList.add('darkcolorfooter');
 	
 
 
-}
+// }
 
-light.onclick = function themeLight() {
-    light.classList.add('hidden');
-	dark.classList.remove('hidden');
-	main.classList.remove('darkcolor');
-	header002.classList.remove('darkcolor');
+// light.onclick = function themeLight() {
+//     light.classList.add('hidden');
+// 	dark.classList.remove('hidden');
+// 	main.classList.remove('darkcolor');
+// 	header002.classList.remove('darkcolor');
 
-	text.classList.remove('lightcolor');
-	pdtitle.classList.remove('lightcolor')
-	topbox.classList.remove('darkcolor')
-	footer.classList.remove('darkcolorfooter')
-
-
-}
+// 	text.classList.remove('lightcolor');
+// 	pdtitle.classList.remove('lightcolor')
+// 	topbox.classList.remove('darkcolor')
+// 	footer.classList.remove('darkcolorfooter')
 
 
-const header002 = document.getElementById('header002')
+// }
+
+
+// const header002 = document.getElementById('header002')
 
 
 // dark.onclick = function aboutDark() {
